0d010b1581fa8789d87491914d17fd9afbd92cf3
Cason A Konzer <72440835+casonk@users.noreply.github.com>
2021-07-23 18:56:46 -0400

    commit plotz to main
    some plotting functions for pulled data

---
+++ Plotz.py
@@ -0,0 +1,42 @@
+'''                                
+Created By Cason Konzer June 2021
+Version 1.0
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+import logo
+import time
+
+
+
+def importcsv():
+    csvfile = input('Please input full csv filename \n')
+    global df
+    df = pd.read_csv(csvfile, low_memory=False)
+
+def plot_date_vs_numcomm(kind):
+    if kind == 'N':
+        df.plot(x='Post Date', y='# Comments')
+    if kind == 'S':
+        df.plot.scatter(x='Post Date', y='# Comments')
+    plt.show()
+
+def plot_date_vs_score(kind):
+    if kind == 'N':
+        df.plot(x='Post Date', y='Score', color='red')
+    if kind == 'S':
+        df.plot.scatter(x='Post Date', y='Score', color='red')
+    plt.show()
+
+def plot_post_vs_numcomm(kind):
+    if kind == 'N':
+        df.plot(x='Parent Id', y='# Comments', color='green')
+    if kind == 'S':
+        df.plot.scatter(x='Parent Id', y='# Comments', color='green')
+    plt.show()
+
+
+if __name__ == '__main__':
+    logo.um()
+    importcsv()
+    plot_date_vs_numcomm('S')
