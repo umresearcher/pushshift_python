33be0b24381cee11496e0db9d15c846b8200b51b
Cason A Konzer <72440835+casonk@users.noreply.github.com>
2021-07-29 14:49:02 -0400

    Information Retrival
    run as main

--- Puller.py
+++ Puller.py
@@ -1,53 +1,70 @@
 '''                                
-Created By Cason Konzer July, 26 2021
-Version 0.1
+Created By Cason Konzer July, 29 2021
+Version 0.3
 '''
 
-import requests, json, time, csv, datetime, Dependencies as dp
+# import modules
+import requests, json, csv, Dependencies as dp
+s = 'submission'; c = 'comment'
 
-s = 'submission'; c = 'comment'; stats = {}
 
 
 
+# prompt user for subreddit to query
 def community():
-    print('<<< What subreddit would you like to query? >>>\n<<< Enter name of subreddit [ex. Research] >>>  ::', end = '')
+    global name; global sub
+    print('<<< What subreddit would you like to query? >>>\n\n<<< Enter name of subreddit [ex. Research] >>> :: ', end = '')
     sub = str(input())
-    global name
     name = sub + '.data.csv'
-    print('\n<<< You have choosen to que subreddit: "', sub, '" >>>\n\n', '---------------------' * 5, '\n')
+    print('\n<<< You have choosen to que subreddit: ', sub, ' >>>\n\n' + ('-----------------------' * 5), '\n')
     return sub, name
 
 
 
 
+# prompt user for earliest post
 def past():
-    print('<<< What is the earliest post you would like to see? *Enter in utc timestamp* >>>\n')
-    print('<<< Enter 0 for earliest availiable post >>>  ::', end = '')
+    while True:
+        print('<<< What is the earliest post you would like to see? *Enter in utc timestamp* >>>\n\n<<< Enter 0 for earliest availiable post >>> :: ', end = '')
+        after = str(input())
 
-    after = str(input())
-    if after == '0':
-        after = '0000000000'
+        if after == '0':
+            after = '0000000000'
+            break
+        elif len(after) == 10:
+            break
+        else:
+            print('>>> INPUT ERROR <<<')    
+            continue
 
-    print('\n<<< You have choosen to que posts after utc: "', after, '" >>>\n\n', '---------------------' * 5, '\n')
+    print('\n<<< You have choosen to que posts after utc: ', after, ' >>>\n\n' + ('-----------------------' * 5), '\n')
     return after
 
 
 
 
+# prompt user for latest post
 def future():
-    print('<<< What is the latest post you would like to see? *Enter in utc timestamp* >>>\n')
-    print('<<< Enter 0 for latest availiable post >>>  ::', end = '')
+    while True:
+        print('<<< What is the latest post you would like to see? *Enter in utc timestamp* >>>\n\n<<< Enter 0 for latest availiable post >>> :: ', end = '')
+        before = str(input())
 
-    before = str(input())
-    if before == '0':
-        before = '9999999999'
+        if before == '0':
+            before = '9999999999'
+            break
+        elif len(before) == 10:
+            break
+        else:
+            print('>>> INPUT ERROR <<<')    
+            continue
     
-    print('\n<<< You have choosen to que posts before utc: "', before, '" >>>\n\n', '---------------------' * 5, '\n')
+    print('\n<<< You have choosen to que posts before utc: ', before, ' >>>\n\n' + ('-----------------------' * 5), '\n')
     return before
 
 
 
 
+# hit pushshift api; grab json and store as 'data'
 def que(a, b, sub, postT):
     global before; global after
     before = b; after = a
@@ -58,12 +75,12 @@ def que(a, b, sub, postT):
         postT = c
 
     url = 'https://api.pushshift.io/reddit/search/{}/?after={}&before={}&subreddit={}&size={}'.format(str(postT), str(after), str(before), str(sub), '123456789')
-    time.sleep(1); print(url)
+    dp.zzz(1); print('\n>> url:', url, '\n')
 
     try:
-        time.sleep(1); r = requests.get(url); status = r.status_code; print('http response is:', status)
+        r = requests.get(url); status = r.status_code; print('>> http response is:', status)
     except:
-        status = ' NO HANDSHAKE '; print('http response is:', status)
+        status = ' NO HANDSHAKE '; print('>> http response is:', status)
 
     if status != 200:
         retry = 0
@@ -74,9 +91,9 @@ def que(a, b, sub, postT):
             dp.loader(retry)
 
             try:
-                r = requests.get(url); status = r.status_code; print('retry http response is:', status)
+                r = requests.get(url); status = r.status_code; print('>> retry http response is:', status)
             except:
-                status = ' NO HANDSHAKE '; print('retry http response is:', status)
+                status = ' NO HANDSHAKE '; print('>> retry http response is:', status)
             
             if status == 200:
                 break
@@ -87,33 +104,37 @@ def que(a, b, sub, postT):
 
 
 
+# write stats from pushshift to csv
 def write(to, fro, sc, cc):
     with open(to, 'w', newline = '', encoding = 'utf-8') as file:
-        a = csv.writer(file, delimiter = ',')
+        w = csv.writer(file, delimiter = ',')
         headers = ["Post Type", "Subreddit", "ID", "Link Id", "Parent Id", "Url", "Permalink", "UTC", "Retrived On", "Post Date", "Score", "# Comments", "Author", "Title", "Body"]
-        a.writerow(headers)
+        w.writerow(headers)
 
         for stat in fro:
-            a.writerow(fro[stat][0])
+            w.writerow(fro[stat][0])
 
-        print('{} submissions, and {} comments have been uploaded into {} \n'.format(str(sc), str(cc), to))
-    time.sleep(1)
+        print('\n>> {} submissions, and {} comments have been uploaded into {} \n'.format(f'{sc:,}', f'{cc:,}', to))
+    dp.zzz(1)
 
 
 
-def call_data(data, postT): # take relevant data from json and write to dictionary 
+
+# call 'data' json object and write selctive attributes to stats dict
+def call_data(data, postT): 
     Data = list() # list to store data points
     PostType = postT
+
     try:
         sub = data['subreddit'] # 
     except KeyError:
         sub = 'NA'
     try:
-        Id = data['id'] # Returns the id of the comment or submission
+        Id = data['id'] #
     except KeyError:
         Id = 'NA'
     try:
-        parId = data['parent_id'] # Returns the id of the parent post
+        parId = data['parent_id'] #
     except KeyError:
         parId = 'NA'
     try:
@@ -121,7 +142,7 @@ def call_data(data, postT): # take relevant data from json and write to dictiona
     except KeyError:
         linkId = 'NA'
     try:
-        url = data['url'] # Returns the url of the post
+        url = data['url'] # 
     except KeyError:
         url = 'NA'
     try:
@@ -129,11 +150,11 @@ def call_data(data, postT): # take relevant data from json and write to dictiona
     except KeyError:
         perma = 'NA'
     try:
-        timestamp = data['created_utc']; date_time = datetime.datetime.fromtimestamp(timestamp); date = date_time.strftime("%m/%d/%Y")
+        timestamp = data['created_utc']; t = dp.fts(timestamp); date = dp.nicetime(t) #
     except KeyError:
         date = 'NA'
     try:
-        created_utc = data['created_utc'] # Returns the time of the post in utc
+        created_utc = data['created_utc'] # 
     except KeyError:
         created_utc = 'NA'    
     try:
@@ -141,51 +162,59 @@ def call_data(data, postT): # take relevant data from json and write to dictiona
     except KeyError:
         retrival = 'NA' 
     try:
-        score = data['score'] # Returns the sum score of the post
+        score = data['score'] # 
     except KeyError:
         score = 'NA'
     try:
-        numComments = data['num_comments'] # Returns the number of comments on the post
+        numComments = data['num_comments'] # 
     except KeyError:
         numComments = 'NA'
     try:
-        title = data['title']; title = r'{}'.format(title) # Returns the title of the post
+        title = data['title']; title = r'{}'.format(title) # 
     except KeyError:
         title = 'NA'
     try:
-        body = data['body']; body = r'{}'.format(body) # Returns the body of the post
+        body = data['body']; body = r'{}'.format(body) # 
     except KeyError:
         body = 'NA'
     try:
-        author = data['author']; author = r'{}'.format(author) # Returns the author of the post
+        author = data['author']; author = r'{}'.format(author) # 
     except KeyError:
         author = 'NA'
+
     Data.append((PostType, sub, Id, linkId, parId, url, perma, created_utc, retrival, date, score, numComments, author, title, body)) 
     stats[Id] = Data
 
 
 
 
+# loop to collect all submissions in given timeframe
 def subs(sub, data, name, sc, cc):
     try:
-        print(sub)
+        sub
     except:
         sub, name = community()
 
     while len(data) > 0:
         for subm in data:
             call_data(subm, s); sc += 1
+
         if len(data) > 0: 
-            print(str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
+            print('\n>> current time:', dp.now())
+            print('\n>> posting time:', str(dp.fts(data[-1]['created_utc'])))
             after = data[-1]['created_utc']
             data = que(after, before, sub, s)
-            time.sleep(5)
-        write(name, stats, sc, cc); print(('*********************' * 5) + '\n')
-    print(('XXXXXXXXXXXXXXXXXXXXX' * 5) + '\n\n<<< submission pull completed; now gathering comments >>>\n\n' + ('XXXXXXXXXXXXXXXXXXXXX' * 5) + '\n')
+            dp.zzz(2.5)
+
+        write(name, stats, sc, cc); dp.zzz(2.5); print(('***********************' * 5) + '\n')
+    print(('XXXXXXXXXXXXXXXXXXXXXXX' * 5) + '\n\n<<< submission pull completed >>>\n\n' + ('XXXXXXXXXXXXXXXXXXXXXXX' * 5) + '\n')
+
+    return sc, cc
 
 
 
 
+# loop to collect all comments in given timeframe
 def coms(sub, data, name, sc, cc):
     try:
         print(sub)
@@ -195,10 +224,109 @@ def coms(sub, data, name, sc, cc):
     while len(data) > 0:
         for comm in data:
             call_data(comm, c); cc += 1
-        if len(data) > 0: # Calls get_pushshift_comm_data() with the created data of the last comment
-            print(str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
+
+        if len(data) > 0:
+            print('\n>> current time:', dp.now())
+            print('\n>> posting time:', str(dp.fts(data[-1]['created_utc'])))
             after = data[-1]['created_utc']
             data = que(after, before, sub, c)
-            time.sleep(5)
-        write(name, stats, sc, cc); print(('*********************' * 5) + '\n')
-    print(('XXXXXXXXXXXXXXXXXXXXX' * 5) + '\n\n<<< comment pull completed >>>\n' + ('XXXXXXXXXXXXXXXXXXXXX' * 5) + '\n')
+            dp.zzz(2.5)
+
+        write(name, stats, sc, cc); dp.zzz(2.5); print(('***********************' * 5) + '\n')
+    print(('XXXXXXXXXXXXXXXXXXXXXXX' * 5) + '\n\n<<< comment pull completed >>>\n\n' + ('XXXXXXXXXXXXXXXXXXXXXXX' * 5) + '\n')
+
+    return sc, cc
+
+
+
+
+# call loops for desired post type
+def pull(subm, comm, sub, name, after, before, sc, cc):
+
+    if subm == True:
+        subdata = que(after, before, sub, 's')
+        sc, cc = subs(sub, subdata, name, sc, cc)
+
+    if comm == True:
+        comdata = que(after, before, sub, 'c')
+        sc, cc = coms(sub, comdata, name, sc, cc)
+
+    print('<<< your program has finished and your data is available in the run directory, stored in /' + name + ' >>>')
+
+
+
+
+# initialize variables and start data grab
+def start(subm, comm):
+    sc = 0; cc = 0
+    global stats; stats = {}
+    sub, name = community()
+    after = past()
+    before = future()
+
+    print('<<< Would you like to abort? (0 = no; else = yes) >>> :: ', end='')
+    abort = input()
+
+    if abort == '0':
+        print('\n' + ('-----------------------' * 5))
+        print('\n<<< Variables Initialized >>>\n\n' + ('#######################' * 5))
+        pull(subm, comm, sub, name, after, before, sc, cc)
+    else:
+        print('\n<<< Your pull has been cancled >>>\n')
+    
+
+
+
+
+# call start; make multiple pulls? 
+def starter():
+    print(('#######################' * 5) + '\n')
+    dp.um()
+    print(('#######################' * 5) + '\n')
+    again = '0'
+
+    while again == '0':
+        t = dp.now()
+
+        while True:
+            print('\n<<< Would you like to retrive reddit submissions? (0 = yes; else = no) >>> :: ', end = '')
+            subm = input()
+
+            if subm == '0':
+                subm = True
+                break
+            else:
+                print('>>> INPUT ERROR <<<')
+                continue
+        print('\n', '-----------------------' * 5, '\n')
+        
+        while True:
+            print('\n<<< Would you like to retrive reddit comment? (0 = yes; else = no) >>> :: ', end = '')
+            comm = input()
+
+            if comm == '0':
+                comm = True
+                break
+            else: 
+                print('>>> INPUT ERROR <<<')
+                continue
+        print('\n', '-----------------------' * 5, '\n')
+        
+        start(subm, comm)
+
+        print("\n<<< This pull's run time >>>")
+        dp.timer(t)
+        print('\n<<< Would you like to make a new query? (0 = yes; else = no) >>> :: ', end = '')
+        again = input()
+
+    print('\n<<< Thank you, Goodbye >>>')
+
+
+
+
+if __name__ == '__main__': 
+    start_time = dp.now()
+    starter()
+    print('\n\n\n<<< Total Run Time >>>')
+    dp.timer(start)
+    exit()
