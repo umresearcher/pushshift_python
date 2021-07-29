313898dc02a858a38e0d7e2e9ec2b3808f2ca144
Cason A Konzer <72440835+casonk@users.noreply.github.com>
2021-07-29 14:49:41 -0400

    Update Dependencies.py

--- Dependencies.py
+++ Dependencies.py
@@ -1,66 +1,119 @@
 '''                                
-Created By Cason Konzer July 26, 2021
-Version 0.0
+Created By Cason Konzer July 27, 2021
+Version 0.1
 '''
-import time, datetime,  Puller as pull
 
+import time, datetime, Puller as pull
+
+
+# Alias
 def now():
     return datetime.datetime.now()
 
-def loader(t):
-    print('\n[                                        ]'); time.sleep(4*t); print('[#                                       ]'); time.sleep(3.9*t)
-    print('[##                                      ]'); time.sleep(3.8*t); print('[###                                     ]'); time.sleep(3.7*t)
-    print('[####                                    ]'); time.sleep(3.6*t); print('[#####                                   ]'); time.sleep(3.5*t)
-    print('[######                                  ]'); time.sleep(3.4*t); print('[#######                                 ]'); time.sleep(3.3*t)
-    print('[########                                ]'); time.sleep(3.2*t); print('[#########                               ]'); time.sleep(3.1*t)
-    print('[##########                              ]'); time.sleep(3*t);   print('[###########                             ]'); time.sleep(2.9*t)
-    print('[############                            ]'); time.sleep(2.8*t); print('[#############                           ]'); time.sleep(2.7*t)
-    print('[##############                          ]'); time.sleep(2.6*t); print('[###############                         ]'); time.sleep(2.5*t)
-    print('[################                        ]'); time.sleep(2.4*t); print('[#################                       ]'); time.sleep(2.3*t)
-    print('[##################                      ]'); time.sleep(2.2*t); print('[###################                     ]'); time.sleep(2.1*t)
-    print('[####################                    ]'); time.sleep(2*t);   print('[#####################                   ]'); time.sleep(1.9*t)
-    print('[######################                  ]'); time.sleep(1.8*t); print('[#######################                 ]'); time.sleep(1.7*t)
-    print('[########################                ]'); time.sleep(1.6*t); print('[#########################               ]'); time.sleep(1.5*t)
-    print('[##########################              ]'); time.sleep(1.4*t); print('[###########################             ]'); time.sleep(1.3*t)
-    print('[############################            ]'); time.sleep(1.2*t); print('[#############################           ]'); time.sleep(1.1*t)
-    print('[##############################          ]'); time.sleep(t);     print('[###############################         ]'); time.sleep(.9*t)
-    print('[################################        ]'); time.sleep(.8*t);  print('[#################################       ]'); time.sleep(.7*t)
-    print('[##################################      ]'); time.sleep(.6*t);  print('[###################################     ]'); time.sleep(.5*t)
-    print('[####################################    ]'); time.sleep(.4*t);  print('[#####################################   ]'); time.sleep(.3*t)
-    print('[######################################  ]'); time.sleep(.4*t);  print('[####################################### ]'); time.sleep(.3*t)
-    print('[########################################]\n'); 
+# Alias
+def fts(ts):
+    return datetime.datetime.fromtimestamp(ts)
 
+# Alias
+def zzz(t):
+    time.sleep(t)
+
+# Alias
+def nicetime(t):
+    t.strftime("%m/%d/%Y")
+
+# Alias
+def extract():
+    pull.starter()
+
+
+
+
+# Run time
+def timer(start):
+    end = now()
+    print('\n\n\nStarted: ' + str(start) + '\n\nFinished: ' + str(end) + '\n\n') 
+
+
+
+
+# Wait loader
+def loader(t):
+    print(  '[#####                                                                                          ]'); zzz(2.0*t)
+    print(  '[#################                                                                              ]'); zzz(2.0*t)
+    print(  '[#########################                                                                      ]'); zzz(1.9*t)
+    print(  '[##############################                                                                 ]'); zzz(1.9*t)
+    print(  '[###################################                                                            ]'); zzz(1.8*t)
+    print(  '[#######################################                                                        ]'); zzz(1.8*t)
+    print(  '[###########################################                                                    ]'); zzz(1.7*t)
+    print(  '[##############################################                                                 ]'); zzz(1.7*t)
+    print(  '[#################################################                                              ]'); zzz(1.6*t)
+    print(  '[####################################################                                           ]'); zzz(1.6*t)
+    print(  '[#######################################################                                        ]'); zzz(1.5*t)
+    print(  '[##########################################################                                     ]'); zzz(1.5*t)
+    print(  '[#############################################################                                  ]'); zzz(1.4*t)
+    print(  '[###############################################################                                ]'); zzz(1.4*t)
+    print(  '[#################################################################                              ]'); zzz(1.3*t)
+    print(  '[###################################################################                            ]'); zzz(1.3*t)
+    print(  '[#####################################################################                          ]'); zzz(1.2*t)
+    print(  '[#######################################################################                        ]'); zzz(1.2*t)
+    print(  '[#########################################################################                      ]'); zzz(1.1*t)
+    print(  '[###########################################################################                    ]'); zzz(1.1*t)
+    print(  '[#############################################################################                  ]'); zzz(1.0*t)
+    print(  '[###############################################################################                ]'); zzz(1.0*t)
+    print(  '[#################################################################################              ]'); zzz(0.9*t)
+    print(  '[###################################################################################            ]'); zzz(0.9*t)
+    print(  '[#####################################################################################          ]'); zzz(0.8*t)
+    print(  '[######################################################################################         ]'); zzz(0.8*t)
+    print(  '[#######################################################################################        ]'); zzz(0.7*t)
+    print(  '[########################################################################################       ]'); zzz(0.7*t)
+    print(  '[#########################################################################################      ]'); zzz(0.6*t)
+    print(  '[##########################################################################################     ]'); zzz(0.6*t)
+    print(  '[###########################################################################################    ]'); zzz(0.5*t)
+    print(  '[###########################################################################################    ]'); zzz(0.5*t)
+    print(  '[############################################################################################   ]'); zzz(0.4*t)
+    print(  '[############################################################################################   ]'); zzz(0.3*t)
+    print(  '[#############################################################################################  ]'); zzz(0.3*t)
+    print(  '[#############################################################################################  ]'); zzz(0.2*t)
+    print(  '[############################################################################################## ]'); zzz(0.2*t)
+    print(  '[############################################################################################## ]'); zzz(0.1*t)
+    print(  '[###############################################################################################]'); zzz(0.1*t)
+    print(  '[###############################################################################################\n]')
+
+
+
+
+# UM 
 def um():
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                         __________________            __________________                           ')
-    print('                        |UUUUUUUUMMMMNNNNNN\          /UUUUUMMMMMNNNNNNNN|                          ')
-    print('                        |UUUUUUUUMMMMNNNNNNN\        /UUUUUUUMMMMNNNNNNNN|                          ')
-    print('                        |UUUUUUUUMMMMNNNNNNNN\      /UUUUUUUUMMMMNNNNNNNN|                          ')
-    print('                          |UUUUUUMMMMNNNNNNNNN\    /UUUUUUUUUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNNNNNNNNNN\__/UUUUUUUUUUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNNNNMMUUUUUUMMMMMMMMUUUUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNNNNMMUUUUUUMMMMMMMMUUUUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNN| \MUUUUUUMMMNNNN/ |UUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNN|  \MMUUUUMMNNNN/  |UUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNN|   \MMUUUMMNNN/   |UUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNN|    \MMUUMMNN/    |UUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNN|     \MMUMNN/     |UUMMMMNNNNNN|                            ')
-    print('                          |UUUUUUMMMMNN|      \MMNN/      |UUMMMMNNNNNN|                            ')
-    print('                         _|UUUUUUMMMMNN|_      \MN/      _|UUMMMMNNNNNN|_                           ')
-    print('                        |UUUUUUUUMMMMNNNN|              |UUUUMMMMNNNNNNNN|                          ')
-    print('                        |UUUUUUUUMMMMNNNN|   UM FLINT   |UUUUMMMMNNNNNNNN|                          ')
-    print('                        |UUUUUUUUMMMMNNNN|   COMP SCI   |UUUUMMMMNNNNNNNN|                          ')
-    print('                        |UUUUUUUUMMMMNNNN|   RESEARCH   |UUUUMMMMNNNNNNNN|                          ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
-    print('                                                                                                    ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                               ___________________              ___________________                                  ')
+    print('                              |UUUUUUUUUMMMMMNNNNN\            /UUUUUMMMMMMNNNNNNNN|                                 ')
+    print('                              |UUUUUUUUUMMMMMNNNNNN\          /UUUUUUMMMMMMNNNNNNNN|                                 ')
+    print('                              |UUUUUUUUUMMMMMNNNNNNN\        /UUUUUUUMMMMMNNNNNNNNN|                                 ')
+    print('                              |UUUUUUUUUMMMMMNNNNNNNN\      /UUUUUUUUMMMMMNNNNNNNNN|                                 ')
+    print('                                 |UUUUUUMMMMMNNNNNNNNN\    /UUUUUUUUUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNNNNNNNNNN\__/UUUUUUUUUUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNNNNMMUUUUUUMMMMMMMMUUUUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNNNNMMUUUUUUMMMMMMMMUUUUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN| \MUUUUUUMMMNNNN/ |UUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN|  \MMUUUUMMNNNN/  |UUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN|   \MMUUUMMNNN/   |UUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN|    \MMUUMMNN/    |UUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN|     \MMUMNN/     |UUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN|      \MMNN/      |UUMMMMMNNNNNN|                                    ')
+    print('                                 |UUUUUUMMMMMNN|       \MN/       |UUMMMMMNNNNNN|                                    ')
+    print('                               __|UUUUUUMMMMMNN|__              __|UUMMMMMNNNNNN|__                                  ')
+    print('                              |UUUUUUUUUMMMMMNNNNN|            |UUUUUMMMMMNNNNNNNNN|                                 ')
+    print('                              |UUUUUUUUUMMMMMNNNNN|  UM FLINT  |UUUUUMMMMMNNNNNNNNN|                                 ')
+    print('                              |UUUUUUUUUMMMMMNNNNN|  COMP SCI  |UUUUUMMMMMNNNNNNNNN|                                 ')
+    print('                              |UUUUUUUUUMMMMMNNNNN|  RESEARCH  |UUUUUMMMMMNNNNNNNNN|                                 ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
+    print('                                                                                                                     ')
